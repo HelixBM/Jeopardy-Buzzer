1a161997443d4c040d2364f2ad9dcb14555cb0f6
Bodhi <95171956+HelixBM@users.noreply.github.com>
2026-07-08 08:35:53 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -6,12 +6,12 @@
 // It is normal that this config is public in a GitHub Pages app.
 // Security comes from Firebase rules, not from hiding this object.
 
-export const firebaseConfig = {
-  apiKey: "PASTE_YOUR_VALUE_HERE",
-  authDomain: "PASTE_YOUR_VALUE_HERE",
-  databaseURL: "PASTE_YOUR_VALUE_HERE",
-  projectId: "PASTE_YOUR_VALUE_HERE",
-  storageBucket: "PASTE_YOUR_VALUE_HERE",
-  messagingSenderId: "PASTE_YOUR_VALUE_HERE",
-  appId: "PASTE_YOUR_VALUE_HERE"
+export  const firebaseConfig = {
+  apiKey: "AIzaSyCV9uuL17dEv1-NXBATi6YDuwVBMEpzLy4",
+  authDomain: "jeopardybuzzer-fbd57.firebaseapp.com",
+  databaseURL: "https://jeopardybuzzer-fbd57-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "jeopardybuzzer-fbd57",
+  storageBucket: "jeopardybuzzer-fbd57.firebasestorage.app",
+  messagingSenderId: "462269228938",
+  appId: "1:462269228938:web:9d85edbeffe5ba96240a73"
 };
